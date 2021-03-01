694da5bf1241b3f9f9468a2dd3314df8672b07c7
bdlowery <16507857+bdlowery@users.noreply.github.com>
2021-02-28 20:26:29 -0800

    finish excercise 18

--- 19-bmi-calculator/app.js
+++ 19-bmi-calculator/app.js
@@ -11,5 +11,16 @@ const bmiCalc = function () {
   //so this will work to verify if either input is a number or not.
   if (isNaN(userHeight + userWeight) === false) {
     bmiTotal = (userWeight / (userHeight * userHeight)) * 703;
+
+    if(bmiTotal >= 18.5 && bmiTotal <= 25) {
+      output.textContent = `Your BMI is ${bmiTotal.toFixed(1)}. You are within the ideal 
+      weight range.`
+    } else if (bmiTotal < 18.5) {
+      output.textContent = `Your BMI is ${bmiTotal.toFixed(2)}, you are underweight. Go see a doctor.`
+    } else if (bmiTotal > 25) {
+      output.textContent = `Your BMI is ${bmiTotal.toFixed(2)}, you are overweight. Go see a doctor.`
+    }
+  } else {
+    output.textContent = `Numbers only.`
   }
 };
